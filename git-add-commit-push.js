10b33d85cfcb7d969fdc786851a05744ce069d86
zkboys <876213774@qq.com>
2020-05-09 11:31:03 +0800

    整理代码

--- git-add-commit-push.js
+++ git-add-commit-push.js
@@ -1,20 +1,28 @@
 // 快速push 到git服务器脚本
-const exec = require('child_process').exec;
+const {execSync} = require('child_process');
 const program = require('commander');
 program
     .version(require('./package').version)
     .usage('[options] <file ...>')
-    .option('-m, --message <说明>  ', '提交说明')
+    .option('-m, --message <注释>  ', '提交注释')
     .parse(process.argv);
 
 if (!program.message) {
     console.error('ERROR:请输入注释！！！');
     return;
 }
-console.log('🔥 push...');
-exec(`git add . && git commit -m '${program.message}' && git push origin master `, function (error, stdout, stderr) {
-    if (error) console.error(error);
 
-    stdout && console.log(stdout);
-    stderr && console.log(stderr);
-});
+execSync('git add .');
+
+execSync(`git commit -m '${program.message}'`);
+
+execSync('git push origin master');
+
+//
+// console.log('🔥 push...');
+// exec(`git add . && git commit -m '${program.message}' && git push origin master `, function (error, stdout, stderr) {
+//     if (error) console.error(error);
+//
+//     stdout && console.log(stdout);
+//     stderr && console.log(stderr);
+// });
